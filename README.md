3d04200fa702637df6ec374a00718c065a3872bb
Lionel Palacin <lionel.palacin@gmail.com>
2023-05-24 12:03:33 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,77 @@
-# relevance-workbench
+# Relevance workbench
+
+This application allows you to compare results ranking between the ELSER model and BM25. 
+
+You can also try with your own data by forking this repo and plugging the application to your own Elasticsearch deployment. 
+
+# Deploy with default dataset
+
+## Pre-requisites
+
+To run this demo successfully, you will need an Elasticsearch deployment (> 8.8) with the ELSER model deployed. The easiest way for this is to use Elastic Cloud as described in the next part but you can also deploy Elasticsearch locally.  
+
+## Deploy Elasticsearch in Elastic Cloud
+
+Start by signing up for a [free Elastic Cloud trial](https://cloud.elastic.co/registration). After creating an account, you’ll have an active subscription, and you’ll be prompted to create your first deployment.
+
+Follow the steps to Create a new deployment. For more details, refer to [Create a deployment](https://www.elastic.co/guide/en/cloud/current/ec-create-deployment.html) in the Elastic Cloud documentation.
+
+Save the default admin password in a safe place. 
+
+## Deploy Elastic Learned Sparse Encoder model
+
+You can follow this [documentation](https://www.elastic.co/guide/en/machine-learning/master/ml-nlp-elser.html#download-deploy-elser) to deploy and start the model on your deployment.
+
+## Load the data
+
+The best approach is to use Enterprise Search to create a new index and configure the ingest pipeline to enrich the data.
+
+From the landing page in Kibana, navigate to Enterprise Search. 
+
+![ent-search-landing](./images/ent-search-landing.png)
+
+Here, click on Create an Elasticsearch index and choose the API method. 
+
+Name the index `search-movies`, and click on Create index. 
+
+Navigate to Pipelines and click on Copy and customize.
+
+![copy-customize](./images/copy-customize.png)
+
+Now click on Add Inference pipeline to configure the inference pipeline. 
+
+Name the inference pipeline `ml-inference-movies` and select the ELSER model. Click continue.
+
+![add-inference-pipeline](./images/add-inference-pipeline.png)
+
+On the next screen, add the fields `overview` and `title` and click Continue. 
+
+![add-inference-fields](./images/add-inference-fields.png)
+
+Click Continue to review the changes and then Create pipeline. 
+
+Go to the folder `data` and run the python script `index-data.py` to ingest the movies dataset. 
+
+```
+python3 index-data.py --es_password=<ELASTICSEARCH_PASSWORD> --cloud_id=<CLOUD_ID>
+```
+
+- ELASTICSEARCH_PASSWORD: Use the default admin password previously saved
+- CLOUD_ID: You can find this information in your Elastic Cloud admin console
+
+## Run the application
+
+The application is composed of a backend Python API and a React frontend. You can run the whole application locally using Docker compose. 
+
+Edit the `docker-compose.yml` file to replace values for. Reuse the same information that you use for loading the data. 
+- CLOUD_ID=<CLOUD_ID>
+- ELASTICSEARCH_PASSWORD=<ELASTICSEARCH_PASSWORD>
+
+Run `docker-compose up` to start the application. 
+
+Open [localhost:3000](http://localhost:3000) to access the application.
+
+
+
+
+
---
+++ app-api/app.py
@@ -0,0 +1,195 @@
+from flask import Flask, request, jsonify
+import os
+from elasticsearch import Elasticsearch 
+
+CLOUD_ID = os.environ["CLOUD_ID"]
+ES_USER = os.environ['ELASTICSEARCH_USERNAME']
+ES_PASSWORD = os.environ['ELASTICSEARCH_PASSWORD']
+
+datasets = {
+    "movies": {
+        "id": "movies",
+        "label": "Movies",
+        "index": "search-movies",
+        "search_fields": ["title", "overview",  "keywords"],
+        "elser_search_fields": ["ml.inference.overview_expanded.predicted_value", "ml.inference.title_expanded.predicted_value^0.5"],
+        "result_fields": ["title", "overview"],
+        "mapping_fields": {"text": "overview", "title": "title"}
+    }
+}
+
+app = Flask(__name__)
+
+@app.route("/api/search/<index>")
+def route_api_search(index):
+    """
+    Execute the search
+    """
+    [query, rrf, type, k, datasetId] = [
+        request.args.get('q'),
+        request.args.get('rrf', default=False, type=lambda v: v.lower() == 'true'),
+        request.args.get('type', default='bm25'),
+        request.args.get('k', default=0),
+        request.args.get('dataset', default='movies')
+    ]
+    if type=='elser':
+        search_result = run_semantic_search(query, index, **{ 'rrf': rrf, 'k': k, 'dataset': datasetId })
+    elif type=='bm25': 
+        search_result = run_full_text_search(query, index, **{ 'dataset': datasetId })
+    transformed_search_result = transform_search_response(search_result,  datasets[datasetId]['mapping_fields'])
+    return jsonify(response=transformed_search_result) 
+
+
+
+@app.route("/api/datasets",  methods=['GET'])
+def route_api_datasets():
+    """
+    Return the available datasets
+    """
+    return datasets
+
+
+@app.errorhandler(404)
+def resource_not_found(e):
+    """
+    Return a JSON response of the error and the URL that was requested
+    """
+    return jsonify(error=str(e)), 404
+
+def get_text_expansion_request_body(query, size = 10, **options):
+    """
+    Generates an ES text expansion search request.
+    """
+    fields = datasets[options['dataset']]['elser_search_fields']
+    result_fields = datasets[options['dataset']]['result_fields']
+    text_expansions = []
+    boost = 1
+   
+    for field in fields:
+
+        split_field_descriptor = field.split("^")
+        if len(split_field_descriptor) == 2: 
+            boost = split_field_descriptor[1]
+            field = split_field_descriptor[0]
+        te = {"text_expansion": {}}
+        te['text_expansion'][field] = {
+            "model_text": query,
+            "model_id": ".elser_model_1",
+            "boost": boost
+          }
+        text_expansions.append(te)
+    return {
+        '_source': False,
+        'fields': result_fields,
+        'size': size,
+        'query': {
+            "bool": {
+                "should": text_expansions
+            }
+        }        
+    }
+
+def get_text_search_request_body(query, size = 10, **options):
+    """
+    Generates an ES full text search request.
+    """
+    fields = datasets[options['dataset']]['result_fields']
+    search_fields = datasets[options['dataset']]['search_fields']
+    return {
+        '_source': False,
+        'fields': fields,
+        'size': size,
+        'query': {
+            "multi_match" : {
+                "query":  query, 
+                "fields": search_fields
+            }
+        }
+    }        
+    
+
+def execute_search_request(index, body):
+    """
+    Executes an ES search request and returns the JSON response.
+    """
+    es = Elasticsearch(
+        cloud_id=CLOUD_ID,
+        basic_auth=(ES_USER,ES_PASSWORD)
+    )
+    response = es.search(index=index,query=body["query"], fields=body["fields"], size=body["size"], source=body["_source"])
+
+    return response
+
+def run_full_text_search(query, index, **options):
+    """
+    Runs a full text search on the given index using the passed query.
+    """
+    if query is None or query.strip() == '':
+        raise Exception('Query cannot be empty')
+    body = get_text_search_request_body(query, **options)
+    response = execute_search_request(index, body)
+
+    return response['hits']['hits']
+
+
+
+def run_semantic_search(query, index, **options):
+    """
+    Runs a semantic search of the provided query on the target index, and reranks the KNN and BM25 results.
+    """
+    elser_body = get_text_expansion_request_body(query, **options)
+    elser_response_json = execute_search_request(index, elser_body)
+    k = int(options['k']) if 'k' in options else 0
+    # Rerank hits using RRF
+    if options.get('rrf') == True:
+        bm25_body = get_text_search_request_body(query, 50, **options)
+        bm25_response_json = execute_search_request(index, bm25_body)
+        reranked_hits = rerank_hits(elser_response_json['hits']['hits'], bm25_response_json['hits']['hits'], k)
+        # Replace final resultset with top 10 reranked hits
+        elser_response_json['hits']['hits'] = reranked_hits[:10]
+
+    return elser_response_json['hits']['hits']
+
+
+def rerank_hits(hits: list, other_hits: list, k: int):
+    """
+    Reranks hits with RRF. `hits` must contain the main list of documents to rerank. Uses the `_id` attribute of the
+    documents to find matching ones in `other_hits`, and `k` as the rank constant.
+    Should be done on the Elasticsearch side when supported: https://github.com/elastic/elasticsearch/issues/84324
+    """
+
+    i = 0
+    for hit in hits:
+        i += 1
+        # Find the matching ordinal of the doc with the same _id in other_hits
+        other_hit_index = find_id_index(hit['_id'], other_hits)
+        rrf_score = 1 / (k + i)
+        if other_hit_index > 0:
+            rrf_score += 1 / (k + other_hit_index)
+        hit['_rrf_score'] = rrf_score
+        hit['_score'] = f'{round(rrf_score, 5)} ({round(1 / (k + i), 5)} + {round(1 / (k + other_hit_index), 5) if other_hit_index > 0 else 0}), KNN #{i}, BM25: #{other_hit_index}'
+
+    hits.sort(key=lambda hit: hit['_rrf_score'], reverse=True)
+    
+    return hits
+
+
+def find_id_index(id: int, hits: list):
+    """
+    Finds the index of an object in `hits` which has _id == `id`.
+    """
+
+    for i, v in enumerate(hits):
+        if v['_id'] == id:
+            return i + 1
+    return 0
+
+def transform_search_response(searchResults, mappingFields):
+    for hit in searchResults:
+        fields = hit['fields']
+        hit['fields'] = {
+            'text': fields[mappingFields['text']],
+            'title': fields[mappingFields['title']]
+        }
+    return searchResults
+
